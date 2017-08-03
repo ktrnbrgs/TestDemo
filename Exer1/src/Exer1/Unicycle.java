1ebff331960fe2e76893c6d46a3eef9921c7afa6
BURGOKA <BURGOKA@ZHA-ITA022-W7.corp.oocl.com>
2017-08-03 16:34:32 +0800

    Exercises-ITA

---
+++ Exer1/src/Exer1/Unicycle.java
@@ -0,0 +1,13 @@
+package Exer1;
+
+public class Unicycle extends Cycle{
+	/*public String ride() {
+		return "Unicycle";
+	}*/
+	
+	//Cycle un = new Unicycle();
+	
+	public String balance() {
+		return "Uni";
+	}
+}
