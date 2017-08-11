2a71f07e8d104b5a01bd773e12318d0a433e98d2
BURGOKA <BURGOKA@ZHA-ITA022-W7.corp.oocl.com>
2017-08-11 17:56:42 +0800

    Exercise august 8

---
+++ Exer0808/Exer0808/src/com/exercise/Class1.java
@@ -0,0 +1,12 @@
+package com.exercise;
+
+
+public class Class1 {
+	public String firstMethod() {
+		return secondMethod();
+	}
+	
+	public String secondMethod() {
+		return "Hey Second Method";
+	}
+}
---
+++ Exer0808/Exer0808/src/com/exercise/ExceptionFound.java
@@ -0,0 +1,12 @@
+package com.exercise;
+
+public class ExceptionFound extends Throwable {
+
+	public ExceptionFound() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ExceptionFound (String string) {
+		super(string);
+	}
+}
