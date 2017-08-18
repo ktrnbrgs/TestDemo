a6de159426f587e27111f1e4aa7f80bf910e9236
Katrina Burgos <katrina.burgos@oocl.com>
2017-08-18 17:39:23 +0800

    August 17

--- AUG16/BURGOKA16AUG/src/com/oocl/burgoka/sw2/Triangle.java
+++
@@ -1,9 +0,0 @@
-package com.oocl.burgoka.sw2;
-
-public class Triangle extends Shape {
-
-	public Triangle() {
-		// TODO Auto-generated constructor stub
-		System.out.println("This is triangle");
-	}
-}
